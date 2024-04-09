4404107b3ac75f705596fd75589436d7af0f3b9b
ashish10010 <thaladipesh89@gmail.com>
2024-04-09 19:26:23 +0545

    home-page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +12,41 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+         title: Row(
+          children: [
+            const SizedBox(width: 8,),
+            Transform.rotate(
+              angle: math.pi / 9.9999,
+              child: Image.asset('assets/images/splash.png',
+              height: 80,
+              width: 60,
+              ),
+            ),
+            const SizedBox(width: 8,),
+            const Text('| AMS',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 33,
+              fontWeight: FontWeight.normal,
+             ),
+            ),
+          ],
+         ),
+      ),
+      body: SafeArea(
+        child: Container(
+          child: const Column(
+            children: [
+               SizedBox(height: 10,)
+            ],
+           ),
+          ),
+        ),
+    );
   }
 }
+
